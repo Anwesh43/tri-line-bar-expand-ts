41401e7917cb47442fbfebd5fa11e7e78c49755c
Anwesh43 <anweshthecool0@gmail.com>
2021-01-26 01:22:31 +0530

    adding TLBENode class

--- index.ts
+++ index.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class TLBENode {
+
+    prev : TLBENode 
+    next : TLBENode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TLBENode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTLBRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TLBENode {
+        var curr : TLBENode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
