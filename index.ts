ad624547cfb40257665f765bbe729a94486cecc2
Anwesh43 <anweshthecool0@gmail.com>
2021-01-26 01:29:51 +0530

    adding TriLineBarExpand class

--- index.ts
+++ index.ts
@@ -184,3 +184,26 @@ class TLBENode {
         return this 
     }
 }
+
+class TriLineBarExpand {
+
+    curr : TLBENode = new TLBENode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *=-1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
